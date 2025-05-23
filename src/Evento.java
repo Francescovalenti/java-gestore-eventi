b13945daf8ac29fe82ec9eaa1c68c4fd95dffbe0
francescovalenti <francescovalenti83@outlook.it>
2025-05-23 19:47:33 +0200

    feat:  inserimento controllo data

--- src/Evento.java
+++ src/Evento.java
@@ -5,10 +5,21 @@ public class Evento {
     private LocalDate data;
     private int postiTotali;
     private int postiPrenotati;
+    private boolean valido;
     
     
 
-public Evento(String titolo, LocalDate data,int postiTotali,int postiPrenotati){
+public Evento(String titolo, LocalDate data,int postiTotali,boolean valido){
+    this.titolo= titolo;
+    this.data = data;
+    this.postiTotali= postiTotali;
     this.postiPrenotati=0;
+
+    if (data.isBefore(LocalDate.now())){
+        System.out.println("La data che è stata inserita non è valida, essendo una data gia passata.");
+        valido=false;
+       return; 
+    }
+        
 }
 }

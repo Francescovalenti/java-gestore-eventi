2aed351adccf9c6cb4a4351feb7952c163f35f43
francescovalenti <francescovalenti83@outlook.it>
2025-05-24 11:05:20 +0200

    feat: fine classe evento con inserimento dello tostring

--- src/Evento.java
+++ src/Evento.java
@@ -1,87 +1,91 @@
+
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Evento {
     private String titolo;
     private LocalDate data;
     private int postiTotali;
     private int postiPrenotati;
-    
-    
+
     // costruttore con controlli
 
-public Evento(String titolo, LocalDate data,int postiTotali,int postiPrenotati){
-    
-    if (data.isBefore(LocalDate.now())){
-       throw new IllegalStateException("La data che è stata inserita non è valida, essendo una data gia passata.");
-        
-       
-    }
+    public Evento(String titolo, LocalDate data, int postiTotali, int postiPrenotati) {
 
-    if(postiTotali<=0){
-        throw new IllegalStateException("Riscrive il numero totale dello stadio,grazie. ");
-    }
-        
+        if (data.isBefore(LocalDate.now())) {
+            throw new IllegalStateException("La data che è stata inserita non è valida, essendo una data gia passata.");
 
-    
-    this.titolo= titolo;
-    this.data = data;
-    this.postiTotali= postiTotali;
-    this.postiPrenotati=0;
+        }
 
-}
-// getter e setter 
-public String getTitolo (){
-    return titolo;
-}
+        if (postiTotali <= 0) {
+            throw new IllegalStateException("Riscrive il numero totale dello stadio,grazie. ");
+        }
 
-public void setTitolo(String titolo){
-    this.titolo=titolo;
-}
-
-public LocalDate getData(){
-    return data;
-}
-
-public void setData(LocalDate data){
-    this.data=data;
-}
-
-public int getpostiTotali(){
-    return postiTotali;
-}
-
-public int getpostiPrenotati(){
-    return postiPrenotati;
-}
-
-// inserimento metodo per prenotare
-public void prenota(){
-    if (data.isBefore(LocalDate.now())){
-        System.out.println("Impossibile prenotare: evento gia passato");
-    }
-    else if(postiPrenotati >= postiTotali){
-        System.out.println("Posti esauriti");
-    }
-    else{
-        postiPrenotati++;
-        System.out.println("Complimenti,posti prenotati con successo.");
-
-    }
-}
-// inserimento metodo per disdire
-public void disdici(){
-    if (data.isBefore(LocalDate.now())){
-        System.out.println("Impossibile disdire: evento gia passato");
-    }
-    else if(postiPrenotati == 0){
-        System.out.println("Nessuna prenotazione da disdire.");
-    }
-    else{
-        postiPrenotati--;
-        System.out.println("Disdetta eseguita con successo.");
+        this.titolo = titolo;
+        this.data = data;
+        this.postiTotali = postiTotali;
+        this.postiPrenotati = 0;
 
     }
 
+    // getter e setter
+    public String getTitolo() {
+        return titolo;
+    }
 
-}
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public int getpostiTotali() {
+        return postiTotali;
+    }
+
+    public int getpostiPrenotati() {
+        return postiPrenotati;
+    }
+
+    // inserimento metodo per prenotare
+    public void prenota() {
+        if (data.isBefore(LocalDate.now())) {
+            System.out.println("Impossibile prenotare: evento gia passato");
+        } else if (postiPrenotati >= postiTotali) {
+            System.out.println("Posti esauriti");
+        } else {
+            postiPrenotati++;
+            System.out.println("Complimenti,posti prenotati con successo.");
+
+        }
+    }
+
+    // inserimento metodo per disdire
+    public void disdici() {
+        if (data.isBefore(LocalDate.now())) {
+            System.out.println("Impossibile disdire: evento gia passato");
+        } else if (postiPrenotati == 0) {
+            System.out.println("Nessuna prenotazione da disdire.");
+        } else {
+            postiPrenotati--;
+            System.out.println("Disdetta eseguita con successo.");
+
+        }
+
+    }
+  // metodo toString per main
+    @Override
+    public String toString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        String formattedDate = data.format(formatter);
+        return "Evento" + titolo + "con capienza di " + postiTotali + "che si svolgerà la data:" + formattedDate
+                + "dove hai acquistato" + postiPrenotati + " biglietti";
+
+    }
 }

111c0c7ea5bbe877c3058325ef693872bacecf97
francescovalenti <francescovalenti83@outlook.it>
2025-05-27 14:55:04 +0200

    feat:estesa la classe, aggiunti gli attributi e impliato getter e setter

--- src/Concerto.java
+++ src/Concerto.java
@@ -1,5 +1,33 @@
-
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class Concerto extends Evento {
-    
+    private float Prezzo;
+    private LocalTime orarioEvento;
+
+    public Concerto (String titolo,LocalDate data,int prezziTotali,float Prezzo,LocalTime orarioEvento){ 
+        super(titolo,data,prezziTotali);
+        this.Prezzo=Prezzo;
+        this.orarioEvento=orarioEvento;
+
+}
+
+
+
+
+    public float getPrezzo() {
+        return this.Prezzo;
+    }
+
+    public void setPrezzo(float Prezzo) {
+        this.Prezzo = Prezzo;
+    }
+
+    public LocalTime getOrarioEvento() {
+        return this.orarioEvento;
+    }
+
+    public void setOrarioEvento(LocalTime orarioEvento) {
+        this.orarioEvento = orarioEvento;
+    }
 }

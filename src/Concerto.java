8c46978975d31d8346e1a9c5b51afcf3b95e0b89
francescovalenti <francescovalenti83@outlook.it>
2025-05-28 15:49:29 +0200

    feat : inserimento metodi dataOra e Prezzo

--- src/Concerto.java
+++ src/Concerto.java
@@ -1,33 +1,67 @@
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class Concerto extends Evento {
-    private float Prezzo;
-    private LocalTime orarioEvento;
+    private double Prezzo;
+    private LocalTime dataOrarioEvento;
 
-    public Concerto (String titolo,LocalDate data,int prezziTotali,float Prezzo,LocalTime orarioEvento){ 
-        super(titolo,data,prezziTotali);
-        this.Prezzo=Prezzo;
-        this.orarioEvento=orarioEvento;
+    public Concerto(String titolo, LocalDate data, int prezziTotali, float Prezzo, LocalTime orarioEvento) {
+        super(titolo, data, prezziTotali);
+        // this.Prezzo = Prezzo;
+        // this.dataOrarioEvento = orarioEvento;
+        setData(data);
+        setOrarioEvento(orarioEvento);
 
-}
+    }
 
-
-
-
-    public float getPrezzo() {
+    public double getPrezzo() {
         return this.Prezzo;
     }
 
-    public void setPrezzo(float Prezzo) {
+    public void setPrezzo(double Prezzo) {
         this.Prezzo = Prezzo;
+
     }
 
-    public LocalTime getOrarioEvento() {
-        return this.orarioEvento;
+    public LocalTime getdataOrarioEvento() {
+        return this.dataOrarioEvento;
     }
 
-    public void setOrarioEvento(LocalTime orarioEvento) {
-        this.orarioEvento = orarioEvento;
+    public void setOrarioEvento(LocalTime dataorarioEvento) {
+        if (dataorarioEvento == null)
+        this.dataOrarioEvento = orarioEvento;
+    }
+
+  
+
+    // Definisci il modello di formattazione
+    public static String prezzoFormattato() {
+          double prezzo = 12345.6789;
+        DecimalFormat df = new DecimalFormat("###,##0.00", new DecimalFormatSymbols(Locale.ITALY));
+        // Formatta il prezzo
+        String prezzoFormattato = df.format(prezzo);
+        return prezzoFormattato;
+
+    }
+
+    public  String dataOrarioFormattato() {
+         LocalDateTime dataOra = LocalDateTime.now();
+
+        // Formattazione in formato "yyyy-MM-dd HH:mm:ss"
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String dataOraFormattata = dataOra.format(formatter);
+        return dataOraFormattata;
+    }
+       
+    
+
+    @Override
+    public String toString() {
+        return getdataOrarioEvento() + "-" + getTitolo() + "-" + getPrezzo();
     }
 }
